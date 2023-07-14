27c596c4d11b67b770e059db17a8705fcca68bc5
Harkii03 <har.kii0324@gmail.com>
2023-07-15 00:20:06 +0900

    レスポンスの取得の追加

---
+++ Okiagari_IoT/library/__led__.py
@@ -0,0 +1,36 @@
+from lib_rpi.rpi_ws281x import Color
+import time
+#Colorを出力
+
+def wheel(pos):
+    """Generate rainbow colors across 0-255 positions."""
+    if pos < 85:
+        return Color(pos * 3, 255 - pos * 3, 0)
+    elif pos < 170:
+        pos -= 85
+        return Color(255 - pos * 3, 0, pos * 3)
+    else:
+        pos -= 170
+        return Color(0, pos * 3, 255 - pos * 3)
+    
+def colorWipe(strip, color, wait_ms=100):
+    """Wipe color across display a pixel at a time."""
+    for i in range(strip.numPixels()):
+        strip.setPixelColor(i, color)
+        strip.show()
+        time.sleep(wait_ms / 1000.0)
+
+def color_rainbow(strip, wait_ms=20, iterations=5):
+    """Draw rainbow that uniformly distributes itself across all pixels."""
+    for j in range(256 * iterations):
+        for i in range(strip.numPixels()):
+            strip.setPixelColor(i, wheel(
+                (int(i * 256 / strip.numPixels()) + j) & 255))
+        strip.show()
+        time.sleep(wait_ms / 1000.0)
+
+        
+#　color code をRGBに変換
+def hex_to_rgb(hex_code):
+    hex_code = hex_code.lstrip('#')
+    return tuple(int(hex_code[i:i+2], 16) for i in (0, 2, 4))
--- Okiagari_IoT/okiagari.py
+++ Okiagari_IoT/okiagari.py
@@ -2,11 +2,11 @@ import os
 import time
 import random
 from lib_rpi.rpi_ws281x import PixelStrip, Color
-from __led__ import colorWipe, hex_to_rgb
+from library.__led__ import colorWipe, hex_to_rgb, color_rainbow
 import RPi.GPIO as GPIO
 from pydub import AudioSegment
 import requests
-import json
+import datetime
 
 # LED strip configuration:
 LED_COUNT = 16        # Number of LED pixels.
@@ -35,16 +35,11 @@ encourage_audio_paths = [
 
 voicevox_core_path = "/home/pi/Okiagari_IoT/voicevox_core-0.11.4/example/python/"
 
-def random_audio():
-    audio = random.choice(encourage_audio_paths)
-    audio = AudioSegment.from_wav(audio)
-    return audio
-
 #PIN 17のボタンの処理
 #直近の5件のタスクを音声でおしえてくれる
 def execute_recent_task(task_data):  
     print("ボタンが押されました")
-    recent_task = task_data  # 仮のタスク
+    recent_task = task_data  
     speaker_id = 1  # ずんだもんのスピーカーID
 
     # タスクの音声合成
@@ -60,14 +55,14 @@ def execute_recent_task(task_data):
     
     #応援文の作成
     audio = random.choice(encourage_audio_paths)
-    audio_en = AudioSegment.from_wav(audio)
+    AudioSegment.from_wav(audio)
     os.system(f"su pi -c \"aplay {audio}\"")
 
 
 #PIN 23のボタンの処理
 #流れている音声を中止して、現在のタスクを音声でおしえてくれる
-def stop_and_alert():
-    recent_task = "英語の宿題" #仮のタスク
+def execute_stop_task():
+    recent_task = task_data[0] 
     speaker_id = 1 #ずんだもんのスピーカーID
     
     os.system("su pi -c \"killall aplay\"")
@@ -83,10 +78,9 @@ def stop_and_alert():
     
     #応援文の作成
     audio = random.choice(encourage_audio_paths)
-    audio_en = AudioSegment.from_wav(audio)
+    AudioSegment.from_wav(audio)
     os.system(f"su pi -c \"aplay {audio}\"")
-    
-    
+
 #GPIOの設定
 BUTTON1_PIN = 17
 GPIO.setmode(GPIO.BCM)
@@ -96,16 +90,27 @@ GPIO.add_event_detect(BUTTON1_PIN, GPIO.FALLING, callback=execute_recent_task, b
 BUTTON2_PIN = 23
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(BUTTON2_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.add_event_detect(BUTTON2_PIN, GPIO.FALLING, callback=stop_and_alert, bouncetime=200)
+GPIO.add_event_detect(BUTTON2_PIN, GPIO.FALLING, callback=execute_stop_task, bouncetime=200)
+
+#音楽の再生
+def play_music():
+    print('音楽を再生しました')
+
+    # POSTリクエストから音楽のIDを取得
+    music = get_set_music()
+    music.export("audio/music/music.wav", format="wav")
+
+    #音楽の再生
+    os.system("su pi -c \"aplay /home/pi/Okiagari_IoT/audio/music/music.wav\"")
+    
 
 #色の変更
-def color():
+def change_color():
     print('色を変更しました')
-    # POSTリクエストから色を取得
- 
-    # 色の文字列をRGBに変換
-    rgb = hex_to_rgb(selected_color)
-    # 色を表示    
+    color = get_set_color() #color code を取得
+    #色の文字列をRGBに変換
+    rgb = hex_to_rgb(color)
+    #色を表示    
     colorWipe(strip, Color(rgb[0], rgb[1], rgb[2]))
 
 
@@ -113,6 +118,8 @@ def turn_off():
     print('LEDを消灯しました')
     colorWipe(strip, Color(0, 0, 0))  
 
+
+#タスクの取得
 def get_recent_tasks():
     server_url = '' #サーバーのURL
     response = requests.get(server_url)
@@ -122,8 +129,59 @@ def get_recent_tasks():
     else:
         print('タスクの取得に失敗しました')
 
+#曲の取得
+def get_set_music():
+    server_url = '' #サーバーのURL
+    response = requests.get(server_url)
+    if response.status_code == 200:
+        music_data = response.json()
+        music = AudioSegment.from_wav(f"audio/music/{music_data['music_name']}")
+        return music
+    else:
+        print('音楽の取得に失敗しました')
+
+#色の取得
+def get_set_color():
+    server_url = '' #サーバーのURL
+    response = requests.get(server_url)
+    if response.status_code == 200:
+        color_data = response.json()
+        return color_data['color']
+    else:
+        print('色の取得に失敗しました')
+
+
 
 while True:
+    
     get_recent_tasks()
+    current_time = datetime.datetime.now()
 
+    for task in task_data:
+        deadline = datetime.datetime.strptime(task['deadline'], '%Y-%m-%d %H:%M:%S')
+
+        # タスクの締め切り時間との差を計算
+        time_difference = deadline - current_time
+
+        
+        if 3 <= time_difference.total_seconds() // 3600 < 4:
+            play_music()
+
+        
+        if 1 <= time_difference.total_seconds() // 3600 < 2:
+            play_music()
+                
+        if 30 <= time_difference.total_seconds() // 60 < 31:
+            play_music()
+
+        
+        if 10 <= time_difference.total_seconds() // 60 < 11:
+            play_music()
+
+        
+        if 5 <= time_difference.total_seconds() // 60 < 6:
+            play_music()
+            color_rainbow()
+
+    time.sleep(5)
     
